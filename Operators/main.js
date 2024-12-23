0dfdcb1f96a0500ebf2f2e8cc309fbefb6a799cf
Ryan James Gregoire <ryangregoire286@gmail.com>
2024-12-23 11:47:42 -0500

    Added And Learned About js Operators

---
+++ Operators/main.js
@@ -0,0 +1,31 @@
+let firstName = "Ryan";
+let lastName = "James";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let val1 = 2;
+let val2 = 2;
+// console.log(val1);
+let val = val2 + 2;
+// console.log(val1);
+// val1 = val1 * 10;
+// console.log(val1);
+// val1 = val1 + "20";
+
+let equals1;
+let equals2;
+
+// equals1 = val1 != 0;
+// equals2 = val2 != 0;
+// let equals1 = val2 == 2;
+// let equals2 = val == 4;
+
+// equals1 = val > 2;
+// equals2 = val1 >= 2;
+equals1 = val1 <= 2;
+equals2 = val2 < 2;
+
+console.log(equals1);
+console.log(equals2);
